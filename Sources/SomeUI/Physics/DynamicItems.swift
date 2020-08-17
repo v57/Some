bf4576474bd2a0ff6805b0ec902f3bc1d3cf4bd8
Dmitry <d@kozlov.ru>
2020-08-17 14:11:41 +0300

    Added ViewDynamicItem that creating physics objects for custom view/layer bounds

---
+++ Sources/SomeUI/Physics/DynamicItems.swift
@@ -0,0 +1,37 @@
+#if canImport(UIKit)
+//
+//  File.swift
+//  
+//
+//  Created by Dmitry Kozlov on 8/16/20.
+//
+
+import UIKit
+
+class ViewDynamicItem: NSObject, UIDynamicItem {
+  unowned var view: UIView
+  init(_ view: UIView) {
+    self.view = view
+  }
+  
+  var center: CGPoint {
+    get { view.center }
+    set { view.center = newValue }
+  }
+
+  var bounds: CGRect { view.bounds }
+
+  var transform: CGAffineTransform {
+    get { view.transform }
+    set { view.transform = newValue }
+  }
+  class LayerBounds: ViewDynamicItem {
+    override var center: CGPoint {
+      get { view.layer.bounds.center }
+      set { view.layer.bounds.center = newValue }
+    }
+    override var bounds: CGRect { view.layer.bounds }
+  }
+}
+
+#endif
