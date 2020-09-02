da45ce70972ff22f5cfd8f30bdd796b37b548bc7
Dmitry <d@kozlov.ru>
2020-09-02 22:28:40 +0300

    Vector2: Added a lot of useful functions

--- Sources/Some/Essentials/Vectors.swift
+++ Sources/Some/Essentials/Vectors.swift
@@ -11,6 +11,16 @@ import Foundation
 public struct Vector2<T> {
   public var a: T
   public var b: T
+  public subscript(_ index: Int) -> T {
+    get { index == 0 ? a : b }
+    set {
+      if index == 0 {
+        a = newValue
+      } else {
+        b = newValue
+      }
+    }
+  }
   public init(sorted a: T, _ b: T, sorting: (T,T)->(Bool)) {
     if sorting(a,b) {
       self.a = a
@@ -27,16 +37,66 @@ public struct Vector2<T> {
   public var array: [T] {
     return [a, b]
   }
+  public mutating func swap() {
+    Swift.swap(&a, &b)
+  }
 }
 extension Vector2: Equatable where T: Equatable { }
 extension Vector2: Hashable where T: Hashable { }
 
-extension Vector2 where T: Equatable {
-  public func opposite(of value: T) -> T {
+public extension Vector2 where T: Equatable {
+  func opposite(of value: T) -> T {
     return value == a ? b : a
   }
-  public func contains(_ value: T) -> Bool {
+  func contains(_ value: T) -> Bool {
     return value == a || value == b
+  }
+  func index(of item: T) -> Int? {
+    switch item {
+    case a:
+      return 0
+    case b:
+      return 1
+    default:
+      return nil
+    }
+  }
+}
+public extension Vector2 where T: ComparsionValue {
+  subscript(value value: T.ValueToCompare) -> T {
+    get { with(value) }
+    set {
+      self[index(of: value)!] = newValue
+    }
+  }
+  func opposite(of value: T.ValueToCompare) -> T {
+    return value == a._valueToCompare ? b : a
+  }
+  mutating func editOpposite(of value: T.ValueToCompare, edit: (inout T)->()) {
+    if value == a._valueToCompare {
+      edit(&b)
+    } else {
+      edit(&a)
+    }
+  }
+  func index(of value: T.ValueToCompare) -> Int? {
+    switch value {
+    case a._valueToCompare:
+      return 0
+    case b._valueToCompare:
+      return 1
+    default:
+      return nil
+    }
+  }
+  func with(_ value: T.ValueToCompare) -> T {
+    return value == a._valueToCompare ? a : b
+  }
+  func find(_ value: T.ValueToCompare) -> T? {
+    return value == a._valueToCompare ? a : value == b._valueToCompare ? b : nil
+  }
+  func contains(_ value: T.ValueToCompare) -> Bool {
+    return value == a._valueToCompare || value == b._valueToCompare
   }
 }
 
