73e7448a393662016852474948c416bbd4a3f800
Dmitry Kozlov <d@kozlov.ru>
2020-11-25 00:26:45 +0300

    Removed unused code from SomeTests

--- Tests/SomeTests/SomeTests.swift
+++ Tests/SomeTests/SomeTests.swift
@@ -46,25 +46,6 @@ final class SomeTests: XCTestCase {
     // results.
     
     // Some of the APIs that we use below are available in macOS 10.13 and above.
-    guard #available(macOS 10.13, *) else {
-      return
-    }
-    
-    let fooBinary = productsDirectory.appendingPathComponent("Some")
-    
-    let process = Process()
-    process.executableURL = fooBinary
-    
-    let pipe = Pipe()
-    process.standardOutput = pipe
-    
-    try process.run()
-    process.waitUntilExit()
-    
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8)
-    
-    XCTAssertEqual(output, "Hello, world!\n")
   }
   
   /// Returns path to the built products directory.
